19700f74aa1ebcde24244b05b47c64fd5683e800
Jackson Romie <jromie0924@gmail.com>
2022-03-05 18:57:52 -0700

    remove commented code

--- turretConfig.py
+++ turretConfig.py
@@ -6,7 +6,6 @@ class TurretConfig:
 
 
     def load_neural_net(self):
-        # net = cv2.dnn.readNet('yolov3.weights', 'cfg/yolov3.cfg') # original yolov3
         net = cv2.dnn.readNet('cfg/yolov3-tiny.weights', 'cfg/yolov3-tiny.cfg') # tiny yolo for speed optimization
         return net
     
