afeb87573a50d6cf959e34d9df1fe1cf28191619
Jackson Romie <jromie0924@gmail.com>
2022-03-09 07:10:15 -0700

    add flag to turn off serial comms

--- serialComm.py
+++ serialComm.py
@@ -6,24 +6,27 @@ from turretConfig import TurretConfig
 class SerialComm:
     def __init__(self) -> None:
         self.arduino = None
-        config = TurretConfig()
+        self.config = TurretConfig()
         failure_count = 0
-        wait_time = config.serial_connect_retry_delay_seconds
-        num_retries = config.serial_connect_num_retries
-        while failure_count < num_retries:
-            try:
-                self.arduino = serial.Serial(
-                    port='/dev/ttyACM0', baudrate=115200, timeout=0.1)
-                break
-            except serial.SerialException:
-                failure_count += 1
-                if failure_count >= num_retries:
-                    raise
-                time.sleep(wait_time)
+        wait_time = self.config.serial_connect_retry_delay_seconds
+        num_retries = self.config.serial_connect_num_retries
+        
+        if self.config.enable_serial_comm:
+            while failure_count < num_retries:
+                try:
+                    self.arduino = serial.Serial(
+                        port='/dev/ttyACM0', baudrate=115200, timeout=0.1)
+                    break
+                except serial.SerialException:
+                    failure_count += 1
+                    if failure_count >= num_retries:
+                        raise
+                    time.sleep(wait_time)
 
     def write_data(self, data):
-        try:
-            self.arduino.write(bytes(data, 'utf-8'))
-        except serial.SerialException:
-            print("Error communicating with turret.")
-            time.sleep(1)
+        if self.config.enable_serial_comm:
+            try:
+                self.arduino.write(bytes(data, 'utf-8'))
+            except serial.SerialException:
+                print("Error communicating with turret.")
+                time.sleep(1)
--- turretConfig.py
+++ turretConfig.py
@@ -5,6 +5,7 @@ class TurretConfig:
     def __init__(self) -> None:
         self.show_object_boxes = False
         self.show_target_points = True
+        self.enable_serial_comm = True  # for diagnostis that don't require Arduino
         self.accepted_classes = ['person']
         self.serial_connect_retry_delay_seconds = 1
         self.serial_connect_num_retries = 10
