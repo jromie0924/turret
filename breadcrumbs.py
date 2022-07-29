d8ec0f3ed4ed62d18095f3589b742a9cbae9903e
Jackson Romie <jromie0924@gmail.com>
2022-07-29 15:02:15 -0600

    add hashmap of breadcrumb history so that duplicates won't be added to the breadcrumbs frame

--- breadcrumbs.py
+++ breadcrumbs.py
@@ -4,9 +4,12 @@ import cv2
 class Breadcrumbs:
   def __init__(self) -> None:
     self.breadcrumbs = []
+    self.hist = dict()
 
   def add_breadcrumb(self, x, y):
-    self.breadcrumbs.append({"x": x, "y": y})
+    if not (x, y) in self.hist:
+      self.breadcrumbs.append({"x": x, "y": y})
+      self.hist[(x, y)] = (x, y)
 
   def show_breadcrumbs(self, frame):
     for breadcrumb in self.breadcrumbs:
@@ -15,4 +18,4 @@ class Breadcrumbs:
 
       cv2.rectangle(frame, (x, y), (x + 2, y + 2), (0, 0, 255), 2)
 
-    cv2.imshow('breadcrumbs', frame)
+    cv2.imshow(f'breadcrumbs - {len(self.breadcrumbs)} data points', frame)
