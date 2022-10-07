dbd64d277c189f213ed926a7103ed24463bc4d3b
Jackson Romie <jromie0924@gmail.com>
2022-10-07 11:35:13 -0600

    force 1920x1080 resolution from camera

--- turretCam.py
+++ turretCam.py
@@ -4,6 +4,11 @@ import cv2
 class TurretCam:
     def __init__(self):
         self.vid = cv2.VideoCapture(0)
+        self.vid.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        width=1920
+        height=1080
+        self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     def capture(self):
         _, frame = self.vid.read()
